68bf56b2617947b568e0adba650dda2e857e3303
Arpit Mohapatra <mohapatra.arpit2003@cet.edu.in>
2023-02-17 04:26:57 +0530

    login and sign in UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 import "./App.css";
+import LoginScreen from "./screens/LoginScreen";
+import { 
+ createBrowserRouter,
+ RouterProvider,
+ Route,
+ createRoutesFromElements} from "react-router-dom";
+
+ const user=null
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="/" element={<HomeScreen />}></Route>
+    </Route>
+  )
+)
 const App = () => {
   return (
-    <div className="app">
-      <HomeScreen />
-    </div>
+   <div className="app">
+    {
+      (!user)?(<LoginScreen />):
+      (<RouterProvider router={router}/>)}
+   </div>
   );
 };
 export default App;
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import netflix_logo from "./images/netflix-logo.png";
+import netflix_avatar from "./images/netflix-avatar.png";
 import "./Nav.css";
 const Nav = () => {
   const [show, setShow] = useState(false);
@@ -17,11 +19,11 @@ const Nav = () => {
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
         <img
-          src="./images/netflix-logo.png"
+          src={netflix_logo}
           alt="netflix logo"
           className="nav_logo"
         />
-        <img src="./images/netflix-avatar.png" alt="" className="nav_avatar" />
+        <img src={netflix_avatar} alt="" className="nav_avatar" />
       </div>
     </div>
   );
